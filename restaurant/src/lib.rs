16cd2dabc2947ffe8253a6ef78958083394a1735
m-88888888 <mochi.h8888@gmail.com>
2024-05-05 12:16:56 +0900

    create mod

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,24 +1,34 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
-    }
+//         fn seat_at_table() {}
+//     }
 
-    mod serving {
-        fn take_order() {}
+//     mod serving {
+//         fn take_order() {}
 
-        fn serve_order() {}
+//         fn serve_order() {}
 
-        fn take_payment() {}
-    }
-}
+//         fn take_payment() {}
+//     }
+// }
 
-use crate::front_of_house::hosting;
+// use crate::front_of_house::hosting;
+
+// pub fn eat_at_restaurant() {
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     front_of_house::hosting::add_to_waitlist();
+//     hosting::add_to_waitlist();
+// }
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
     hosting::add_to_waitlist();
 }
