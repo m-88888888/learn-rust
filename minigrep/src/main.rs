47e3db4c789966ba64136d511c76f3bfcda2984e
m-88888888 <mochi.h8888@gmail.com>
2024-05-06 11:36:19 +0900

    refactor: handle Result

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -15,7 +15,11 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(r) = run(config) {
+        println!("Application error: {}", r);
+
+        process::exit(1);
+    }
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
