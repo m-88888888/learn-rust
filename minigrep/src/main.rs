4d1a91f4198a1f4b440cfdf78fb6da669f298ab2
m-88888888 <mochi.h8888@gmail.com>
2024-05-06 11:17:33 +0900

    improve error handling in main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs::File;
-use std::io::prelude::*; // I/Oに関するトレイトを使うためのインポート
+use std::io::prelude::*;
+use std::process; // I/Oに関するトレイトを使うためのインポート
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("In file {}", config.filename);
 
@@ -24,14 +28,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
